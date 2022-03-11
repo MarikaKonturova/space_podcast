0a14dbf26551fbb7c5d8335a5bf2ccc75adebe40
MarikaKonturova <mashakonturova@gmail.com>
2022-03-11 17:34:32 +0300

    header fixes and read.md

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import facebook from "../assets/icons/facebook.svg";
 import instagram from "../assets/icons/instagram.svg";
 import spotify from "../assets/icons/spotify.svg";
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
+
+  const Close = () => setOpen(false);
+
+  const handleClick = () => setOpen(!open);
+
   return (
     <header
-      className={`header ${open ? `header_small_on` : `header_small_off`}`}
+      className={`header header_small_off ${open ? `header_small_on` : ``}`}
     >
-      <div className="wrapper header_wrapper">
+      <div className="wrapper header_wrapper" >
         <div
           className={!open ? "burger" : "close"}
           onClick={() => setOpen(!open)}
@@ -20,17 +25,17 @@ export const Header = () => {
         </div>
         <ul className="header_features">
           <li className="header_feature">
-            <a href="#intro" onClick={() => setOpen(!open)}>
+            <a href="#intro" onClick={handleClick}>
               <h5>Home</h5>
             </a>
           </li>
           <li className="header_feature">
-            <a href="#plans" onClick={() => setOpen(!open)}>
+            <a href="#plans" onClick={handleClick}>
               <h5>Plans</h5>
             </a>
           </li>
           <li className="header_feature">
-            <a href="#singers" onClick={() => setOpen(!open)}>
+            <a href="#singers" onClick={handleClick}>
               <h5>Singers</h5>
             </a>
           </li>

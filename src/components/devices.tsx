a39196f011ff2079ec859dac6e11d0025fcd69e8
MarikaKonturova <mashakonturova@gmail.com>
2023-01-23 18:34:54 +0300

    fix image size and some app fixes

--- src/components/devices.tsx
+++ src/components/devices.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import phone from '../assets/img/phone.svg'
-import desktop from '../assets/img/desktop.svg'
+import React from "react";
+import phone from "../assets/img/phone.png";
+import desktop from "../assets/img/desktop.png";
 
 export const Devices = () => {
   return (
     <section className="devices">
-        <div className="wrapper devices_wrapper">
+      <div className="wrapper devices_wrapper">
         <div className="devices_main">
-            <h2>Listen on desktop or mobile</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. uisque ullamcorper porttitor blandit. Praesent lorem magna, fring
-            </p>
+          <h2>Listen on desktop or mobile</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. uisque
+            ullamcorper porttitor blandit. Praesent lorem magna, fring
+          </p>
         </div>
-        <img src={phone} alt="phone" className="devices_phone"/>
-        <img src={desktop} alt="desktop" className="devices_desktop"/>
-        </div>
+        <img src={phone} alt="phone" className="devices_phone" />
+        <img src={desktop} alt="desktop" className="devices_desktop" />
+      </div>
     </section>
-  )
-}
+  );
+};
